4e72f199b36e2eba624b390359cc1ee89476f235
Huyh Huyhu <2509roblox@gmail.com>
2024-09-26 10:46:03 +0700

    done client layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Livewire\Candidate\Dashboard;
+use App\Livewire\CongTy;
+use App\Livewire\Index;
+use App\Livewire\TuyenDung;
+use App\Livewire\ViecLam;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Index::class)->name('index');
+Route::get('/viec-lam/{slug}', ViecLam::class);
+Route::get('/tuyen-dung/{slug}', TuyenDung::class);
+Route::get('/cong-ty/{slug}', CongTy::class);
+
+
+Route::get('/candidate/dashboard', Dashboard::class);
